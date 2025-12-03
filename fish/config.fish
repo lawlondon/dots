8fdd530e139184fca3cb44d2c4c2efdc97d7a086
London W <londonwallace@proton.me>
2025-12-03 18:31:46 +0800

    reinstalling hyprland

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
 	function fish_greeting
 		hyfetch
 	end
-	alias rebuild-switch 'sudo nixos-rebuild switch --flake ~/dotfiles/nixos/'
-	alias rebuild-boot 'sudo nixos-rebuild boot --flake ~/dotfiles/nixos/'
-	alias upgrade-switch 'nix flake update --flake ~/dotfiles/nixos/ && sudo nixos-rebuild switch --flake ~/dotfiles/nixos/'
-	alias upgrade-boot 'nix flake update --flake ~/dotfiles/nixos/ && sudo nixos-rebuild boot --flake ~/dotfiles/nixos/'
-	alias dots 'cd ~/dotfiles/ && nvim ~/dotfiles/'
+	alias dots 'cd ~/dots/ && nvim ~/dots/'
+	alias update 'yay'
 end
